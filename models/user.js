422d51fafeccd03da6f94666c80f569946fdd155
Brendan <brendann.ch@gmail.com>
2020-11-01 04:21:16 -0800

    add default date to user schema

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const user = new mongoose.Schema({
   userId: String,  // id of the user; doesn't change
   currency: { type: Number, min: 0 },
-  lastClaimedDaily: Date  // user will be able to claim currency daily
+  lastClaimedDaily: { type: Date, default: new Date() },  // user will be able to claim currency daily
 });
 
 module.exports = mongoose.model('user', user);
