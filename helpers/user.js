e73ab9729827031c401a5cf551400ecd841341a1
Brendan <brendann.ch@gmail.com>
2020-11-02 14:28:48 -0800

    add server management for user

--- helpers/user.js
+++ helpers/user.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 // amount of currency user starts with
 const defaultCurrency = process.env.DEFAULT_CURRENCY;
 
-const findUser = async (userId, createNewIfNull = true) => {
+const findUser = async (userId, createNewIfNull = true, serverId = undefined, client = undefined) => {
   // returns null if no user
   const user = await User.findOne({
     userId: userId
@@ -24,6 +24,40 @@ const findUser = async (userId, createNewIfNull = true) => {
         user[value] = userValidator[value];  // update user document with default key
       }
     });
+
+    // from here on we can assume that the server list exists
+    // loop through server list and check if user is still in each server
+    await new Promise(async (resolve, reject) => {
+      if (client) {
+        for (const id of user.servers) {
+          const index = user.servers.indexOf(id);
+
+          let server = undefined;
+          let member = undefined;
+  
+          // server may or may not exist/bot may be missing access
+          try {
+            server = await client.guilds.fetch(id);
+            member = server ? await server.members.fetch(userId) : undefined;
+          } catch(err) {
+            console.error(err);
+          };
+  
+          if (!member || !server) {
+            user.servers.splice(index);  // remove server from list
+          }
+        };
+
+        resolve();
+      } else {
+        resolve();
+      };
+    })
+
+    // if server ID is provided, we check if it exists in server list and add it if it doesn't exist
+    if (serverId && !user.servers.includes(serverId)) {
+      user.servers.push(serverId);  // add the server ID to the list
+    };
 
     await user.save();
 
