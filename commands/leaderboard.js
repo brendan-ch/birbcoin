612e2f4980d03f26a7212cc54000869f62230642
Brendan <brendann.ch@gmail.com>
2020-11-02 13:06:56 -0800

    add leaderboard command

---
+++ commands/leaderboard.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+const { findServer } = require('../helpers/server');
+const { findUser } = require('../helpers/user');
+
+module.exports = {
+  name: "leaderboard",
+  description: "See who the richest people in your server are.",
+  execute: async (message, args) => {
+    const server = message.guild;
+
+    
+  }
+};
