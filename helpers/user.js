60adda67a4d56f9e7197a5ee00cb5ef164caadb2
Brendan <brendann.ch@gmail.com>
2020-11-02 13:11:25 -0800

    add user validator to check user document

--- helpers/user.js
+++ helpers/user.js
@@ -9,12 +9,31 @@ const findUser = async (userId, createNewIfNull = true) => {
   }).exec();
 
   if (user) {  // user found
+    // what we'll use to check if user document is updated
+    // not all keys are here, because we can't set default values for all of them
+    const userValidator = {
+      "servers": []
+    };
+
+    const userKeys = Object.keys(user.toObject());
+    const userValidatorKeys = Object.keys(userValidator);
+
+    // loop through validator keys
+    userValidatorKeys.forEach(value => {
+      if (!userKeys.includes(value)) {
+        user[value] = userValidator[value];  // update user document with default key
+      }
+    });
+
+    await user.save();
+
     return user;
   } else if (createNewIfNull) {  // no user found, create new user in database
     const newUser = new User({
       userId: userId,
       currency: defaultCurrency,
       lastClaimedDaily: new Date(Date.now() - 86400000),  // sets it to one day before so user can claim on creation
+      servers: []
     });
 
     // save new user
