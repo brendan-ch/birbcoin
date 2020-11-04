f5796882341d8fe67c498e869772fcd74ece0e95
Brendan <brendann.ch@gmail.com>
2020-11-03 21:13:12 -0800

    update user schema with username

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const user = new mongoose.Schema({
   userId: String,  // id of the user; doesn't change
   currency: { type: Number, min: 0 },
   lastClaimedDaily: { type: Date, default: new Date() },  // user will be able to claim currency daily
-  servers: { type: Array, default: [] }  // list of server IDs the user has joined
+  servers: { type: Array, default: [] },  // list of server IDs the user has joined
+  usernameDiscord: String
 });
 
 module.exports = mongoose.model('user', user);
