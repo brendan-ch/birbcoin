0f20e222cd5483819a5030b37ae4498c1bbee2e4
Brendan <brendann.ch@gmail.com>
2020-11-03 21:14:07 -0800

    disable server deregistration and add username

--- helpers/user.js
+++ helpers/user.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 // amount of currency user starts with
 const defaultCurrency = process.env.DEFAULT_CURRENCY;
 
-const findUser = async (userId, createNewIfNull = true, serverId = undefined, client = undefined) => {
+const findUser = async (userId, username, createNewIfNull = true, serverId = undefined, client = undefined) => {
   // returns null if no user
   const user = await User.findOne({
     userId: userId
@@ -11,8 +11,9 @@ const findUser = async (userId, createNewIfNull = true, serverId = undefined, cl
   if (user) {  // user found
     // what we'll use to check if user document is updated
     // not all keys are here, because we can't set default values for all of them
+    // the username is set below this because we need to update username regardless of whether it exists already
     const userValidator = {
-      "servers": []
+      "servers": [],
     };
 
     const userKeys = Object.keys(user.toObject());
@@ -25,34 +26,42 @@ const findUser = async (userId, createNewIfNull = true, serverId = undefined, cl
       }
     });
 
+    // update user's username, in case it changed
+    user.usernameDiscord = username;
+
+    // Changing the way server deregistration works:
+    // In the future, server admins will be able to deregister users if they see someone they don't want on the leaderboard
+    // Users will be able to deregister themselves from servers as well (will have to deregister from all servers)
+    // For now, this code is being disabled to avoid unnecessary calls to the Discord API, which may result in errors anyways
+
     // from here on we can assume that the server list exists
     // loop through server list and check if user is still in each server
-    await new Promise(async (resolve, reject) => {
-      if (client) {
-        for (const id of user.servers) {
-          const index = user.servers.indexOf(id);
+    // await new Promise(async (resolve, reject) => {
+    //   if (client) {
+    //     for (const id of user.servers) {
+    //       const index = user.servers.indexOf(id);
 
-          let server = undefined;
-          let member = undefined;
+    //       let server = undefined;
+    //       let member = undefined;
   
-          // server may or may not exist/bot may be missing access
-          try {
-            server = await client.guilds.fetch(id);
-            member = server ? await server.members.fetch(userId) : undefined;
-          } catch(err) {
-            console.error(err);
-          };
+    //       // server may or may not exist/bot may be missing access
+    //       try {
+    //         server = await client.guilds.fetch(id);
+    //         member = server ? await server.members.fetch(userId) : undefined;
+    //       } catch(err) {
+    //         console.error(err);
+    //       };
   
-          if (!member || !server) {
-            user.servers.splice(index);  // remove server from list
-          }
-        };
+    //       if (!member || !server) {
+    //         user.servers.splice(index);  // remove server from list
+    //       }
+    //     };
 
-        resolve();
-      } else {
-        resolve();
-      };
-    })
+    //     resolve();
+    //   } else {
+    //     resolve();
+    //   };
+    // })
 
     // if server ID is provided, we check if it exists in server list and add it if it doesn't exist
     if (serverId && !user.servers.includes(serverId)) {
@@ -67,7 +76,8 @@ const findUser = async (userId, createNewIfNull = true, serverId = undefined, cl
       userId: userId,
       currency: defaultCurrency,
       lastClaimedDaily: new Date(Date.now() - 86400000),  // sets it to one day before so user can claim on creation
-      servers: []
+      servers: [],
+      usernameDiscord: username
     });
 
     // save new user
