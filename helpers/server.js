60ec60e1bb25ebc57792ed41b6ec51d28176e37a
Brendan <brendann.ch@gmail.com>
2020-11-08 05:06:12 -0800

    add server validator and disabled commands

--- helpers/server.js
+++ helpers/server.js
@@ -22,11 +22,30 @@ const findServer = async (serverId) => {
   }).exec();
 
   if (server) {  // a server was found
+    // what we'll use to check if server document is updated
+    // not all keys from the schema are in here, because we can't set default values for some fields
+    const serverValidator = {
+      "prefix": defaultPrefix,
+      "disabledCommands": [],
+    };
+
+    const serverKeys = Object.keys(server.toObject());  // get all keys of server document
+    const serverValidatorKeys = Object.keys(serverValidator);  // get keys of serverValidator (to validate serverKeys)
+
+    // loop through validator keys
+    serverValidatorKeys.forEach(value => {
+      if (!serverKeys.includes(value)) {
+        server[value] = serverValidator[value];  // update server document with "default" value
+      }
+    });
+
+    await server.save();
     return server;
   } else {  // no server was found, create new document in database
     const newServer = new Server({
       serverId: serverId,
       prefix: defaultPrefix,
+      disabledCommands: [],
     });
 
     // save new server
