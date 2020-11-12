a0ee9709b74da2f5a38cd57c94e03b147dd518f6
Brendan <brendann.ch@gmail.com>
2020-11-12 09:56:56 -0800

    add db-related helper functions

---
+++ helpers/blackjack.js
@@ -0,0 +1,114 @@
+const Blackjack = require('../models/blackjack');
+const decks = process.env.BLACKJACK_DECKS || 1;
+
+const constructDeck = (numDecks = 1) => {
+  const suits = ["H", "S", "C", "D"];
+  const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+  let deck = [];
+  for (let i = 1; i <= numDecks; i += 1) {
+    suits.forEach(suit => {
+      values.forEach(value => {
+        deck.push({
+          suit: suit,
+          value: value
+        })  // push every combination to deck
+      })
+    });
+  };
+
+  return deck;
+};
+
+const shuffleDeck = (deck) => {
+  let newDeck = deck.slice(0);
+
+  for (let i = newDeck.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [newDeck[i], newDeck[j]] = [newDeck[j], newDeck[i]];
+  };
+
+  return newDeck;
+};
+
+const updateHand = (game, hand = "player") => {
+  // update player's or dealer's hand
+  if (hand === "player") {
+    game.playerHand.push(game.deck[0]);
+  } else if (hand === "dealer") {
+    game.dealerHand.push(game.deck[0]);
+  };
+
+  game.deck.shift();
+}
+
+// gets an existing game, or create a new one if it doesn't exist
+// run on .blackjack or .blackjack currency
+const findCreateGame = async (userID, bet = 0) => {
+  const game = await Blackjack.findOne({
+    userId: userID
+  }).exec();
+
+  if (game) {
+    return game;
+  } else if (bet > 0) {
+    // create new game document
+    let newGame = new Blackjack({
+      userId: userID,
+      bet: bet,
+      deck: shuffleDeck(constructDeck(decks)),
+      playerHand: [],
+      dealerHand: [],
+      splitHand: [],
+      showDealerCards: false
+    });
+
+    // we'll update player and dealer hands first before saving/returning
+    updateHand(newGame, "player");
+    updateHand(newGame, "dealer");
+    updateHand(newGame, "player");
+    updateHand(newGame, "dealer");
+
+    await newGame.save();
+
+    return newGame;
+  } else {
+    return null;
+  }
+};
+
+// class Blackjack {
+//   constructor(message, bet, decks = 1) {
+//     this.userID = message.userID;  // will be used to identify game
+//     this.bet = bet;  // amount of currency the user is betting
+    
+//     this.deck = shuffleDeck(constructDeck(decks));  // shuffle deck now to make things easier later
+//     this.playerHand = [];
+//     this.dealerHand = [];
+
+//     this.message = message;
+//   };
+
+//   startGame() {
+//     this.updateHand("dealer");
+//     this.updateHand("player");
+//     this.updateHand("dealer");
+//     this.updateHand("player");
+//   }
+
+//   updateHand(hand = "player") {
+//     // take the first card in the deck, move it to player's/dealer's hand
+//     if (hand === "player") this.playerHand.push(this.deck[0]);
+//     else if (hand === "dealer") this.dealerHand.push(this.deck[0]);
+    
+//     this.deck.shift();
+//   };
+// };
+
+// // const newGame = new Blackjack("aaaaa", 50, 1);
+// newGame.startGame();
+// console.log(newGame.playerHand);
+// console.log(newGame.dealerHand);
+
+module.exports.findCreateGame = findCreateGame;
+module.exports.updateHand = updateHand;
